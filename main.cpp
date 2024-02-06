d05602a0d7312a465bff265d621ecd7404605847
Sanjeev Kotha <kothasanju@gmail.com>
2024-02-06 08:38:57 -0700

    Finished phase 4.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <sys/time.h>
+#include <queue>
+//#define DEBUG
 using namespace std;
 
 #ifdef __APPLE__
@@ -65,15 +67,33 @@ int pthread_barrier_wait(pthread_barrier_t *barrier)
 
 long arraySize;
 int threadCount;
+long * values;
+vector<vector<pair<long, long>>> globalPartitions;
 pthread_barrier_t mybarrier;
 
 struct ThreadControlBlock {
     int id;
     long *localBlock;
     long blockSize;
-    long *regularSample;
-    vector<long> pivots;
-    vector<pair<int, int>> partitions;
+    vector<pair<long, long>> regularSample;
+    vector<pair<long, long>> pivots;
+    vector<pair<long, long>> partitions;
+    vector<pair<long, long>> truePartitions;
+    vector<long> mergedPartition;
+};
+
+class QueueObject
+{
+public:
+    long value;
+    long start;
+    long end;
+    QueueObject(long value, long start, long end) {
+        this->value = value;
+        this->start = start;
+        this->end = end;
+    }
+    long getValue() { return value; }
 };
 
 long timeDiff (timeval tv2, timeval tv) {
@@ -117,11 +137,10 @@ void * sortAndSampleLocal(void * arg) {
 
     qsort(block, size, sizeof(long), comparator);
 
-    long * regularSample = myTCB->regularSample;
     long w = arraySize / (threadCount * threadCount);
 
     for (int i = 0; i < threadCount; i++) {
-        regularSample[i] = block[i * w];
+        myTCB->regularSample.emplace_back(block[i * w], (i * floor(arraySize / threadCount)) + (i * w));
     }
 
     pthread_barrier_wait(&mybarrier);
@@ -133,14 +152,62 @@ void *createPartitions(void *arg) {
     long *block = myTCB->localBlock;
     long start = 0;
     long end;
-
-    for (long pivot : myTCB->pivots) {
-        end = binary_search(block, myTCB->blockSize, pivot);
+    for (auto pivot : myTCB->pivots) {
+        end = binary_search(block, myTCB->blockSize, pivot.first);
         myTCB->partitions.emplace_back(start, end);
+        myTCB->truePartitions.emplace_back(start + myTCB->id * floor(arraySize / threadCount), end + myTCB->id * floor(arraySize / threadCount));
         start = end + 1;
     }
 
     myTCB->partitions.emplace_back(start, myTCB->blockSize - 1);
+    myTCB->truePartitions.emplace_back(start + myTCB->id * floor(arraySize / threadCount), myTCB->blockSize - 1 + myTCB->id * floor(arraySize / threadCount));
+
+    pthread_barrier_wait(&mybarrier);
+    pthread_exit(nullptr);
+}
+
+void *exchangePartitions(void *arg) {
+    auto *myTCB = (struct ThreadControlBlock *)arg;
+    for (int i = 0; i < threadCount; i++) {
+        globalPartitions[i].push_back(myTCB->truePartitions[i]);
+    }
+
+    pthread_barrier_wait(&mybarrier);
+    pthread_exit(nullptr);
+}
+
+class minComp {
+public:
+    int operator() (QueueObject q1, QueueObject& q2) {
+        return q1.getValue() > q2.getValue();
+    }
+};
+
+void *successiveMerge(void *arg) {
+    auto *myTCB = (struct ThreadControlBlock *)arg;
+
+    priority_queue <QueueObject, vector<QueueObject>, minComp> pq;
+    vector<long> result;
+    for (auto & i : globalPartitions[myTCB->id]) {
+        if (i.first <= i.second) {
+            long t = *(values + i.first);
+            pq.emplace(t, i.first, i.second);
+        }
+    }
+
+    while (!pq.empty()) {
+        QueueObject smallest = pq.top();
+        pq.pop();
+        result.push_back(smallest.getValue());
+        long start = smallest.start;
+        long end = smallest.end;
+        if (start < end) {
+            long t = *(values + start + 1);
+            pq.emplace(t, start + 1, end);
+        }
+    }
+
+    myTCB->mergedPartition.insert(myTCB->mergedPartition.end(), result.begin(), result.end());
 
     pthread_barrier_wait(&mybarrier);
     pthread_exit(nullptr);
@@ -157,16 +224,47 @@ int main(int argc, char ** argv) {
     // Setting parameters
     arraySize = stol(argv[1]);
     threadCount = stoi(argv[2]);
-    auto * data = new long [arraySize];
+
+    for (int i = 0; i < threadCount; i++) {
+        globalPartitions.emplace_back();
+    }
+
+    values = new long [arraySize];
     struct ThreadControlBlock TCB[threadCount];
     pthread_t ThreadID[threadCount];
     long blockSize = floor(arraySize / threadCount);
     pthread_setconcurrency(threadCount);
     // "Randomly" generating data
     srandom((unsigned) time(nullptr));
+
+#ifdef DEBUG
+    cout << "Original ";
+#endif
     for (long i = 0; i < arraySize; i++) {
-        data[i] = (long) (random());
+        values[i] = (long) (random());
+#ifdef DEBUG
+        cout << values [i] << " ";
+#endif
     }
+
+
+    long *arr_copy = new long[arraySize];
+    //cout << "Original ";
+    for (long i = 0; i < arraySize; i++) {
+        arr_copy[i] = values[i];
+        //cout << values[i] << " ";
+    }
+    cout << endl;
+
+    sort(arr_copy, arr_copy + arraySize);
+
+#ifdef DEBUG
+    cout << "Sorted ";
+    for (long i = 0; i < arraySize; i++) {
+        cout << arr_copy[i] << " ";
+    }
+    cout << endl;
+#endif
 
     struct timeval start_time{};
     gettimeofday(&start_time, nullptr);
@@ -175,8 +273,7 @@ int main(int argc, char ** argv) {
     // Phase 1
     for (int i = 0; i < threadCount; i++) {
         TCB[i].id = i;
-        TCB[i].localBlock = &data[i * blockSize];
-        TCB[i].regularSample = new long[threadCount];
+        TCB[i].localBlock = &values[i * blockSize];
 
         if (i == threadCount - 1) // Incase of uneven partitions
             TCB[i].blockSize = arraySize - (i * blockSize);
@@ -196,20 +293,49 @@ int main(int argc, char ** argv) {
     gettimeofday(&phase_1, nullptr);
     cout << "Time taken for phase 1: " << timeDiff(phase_1, start_time) << " microseconds" << endl;
 
-    vector<long> localSamples;
+    vector<pair<long, long>> localSamples;
+
 
     for (int i = 0; i < threadCount; i++) {
-        localSamples.insert(localSamples.end(), TCB[i].regularSample, TCB[i].regularSample + threadCount);
-        delete TCB[i].regularSample;
+#ifdef DEBUG
+        cout << "Thread " << i << " Local Sorted" <<endl;
+        for (long k = 0; k < TCB[i].blockSize; k++) {
+            cout << TCB[i].localBlock[k] << " ";
+        }
+        cout << endl;
+
+        cout << "Thread " << i << " Samples" <<endl;
+#endif
+        localSamples.insert(localSamples.end(), TCB[i].regularSample.begin(), TCB[i].regularSample.end());
+#ifdef DEBUG
+        for (int j = 0; j < threadCount; j++) {
+            cout << TCB[i].regularSample[j].first <<  " " << "Index " << TCB[i].regularSample[j].second;
+        }
+        cout << endl << endl;
+#endif
     }
 
     sort(localSamples.begin(), localSamples.end());
 
-    long rho = floor(threadCount / 2);
-    vector<long> pivots;
+#ifdef DEBUG
+    cout << "Sorted Gathered Sample  "<<endl;
+    for (int i = 0 ; i < threadCount * threadCount; i++) {
+        cout << localSamples[i].first << " " << "Index " << localSamples[i].second;
+    }
+    cout << endl;
+    cout << endl;
+#endif
 
+    long rho = floor(threadCount / 2);
+    vector<pair<long, long>> pivots;
+#ifdef DEBUG
+    cout << "Pivots "<<endl;
+#endif
     for (int i = 1; i < threadCount; i++) {
-        pivots.push_back(localSamples[i * threadCount + rho]);
+#ifdef DEBUG
+        cout << localSamples[i * threadCount + rho -1].first << " " << "Index " << localSamples[i * threadCount + rho -1].second ;
+#endif
+        pivots.push_back(localSamples[i * threadCount + rho - 1]);
     }
 
     pthread_barrier_init(&mybarrier, nullptr, threadCount);
@@ -225,7 +351,107 @@ int main(int argc, char ** argv) {
 
     pthread_barrier_destroy(&mybarrier);
 
+#ifdef DEBUG
+    for (int i = 0; i < threadCount; i++) {
+        cout << "Partition thread " << i << endl;
+        for (auto p: TCB[i].truePartitions) {
+            for (long l = p.first; l <= p.second; l++) {
+                cout<< values[l] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+#endif
+
     struct timeval phase_2{};
     gettimeofday(&phase_2, nullptr);
     cout << "Time taken for phase 2: " << timeDiff(phase_2, phase_1) << " microseconds" << endl;
+
+    pthread_barrier_init(&mybarrier, nullptr, threadCount);
+    for (int i = 0; i < threadCount; i++) {
+        pthread_create(&(ThreadID[i]), nullptr, exchangePartitions, (void *)&TCB[i]);
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(ThreadID[i], nullptr);
+    }
+
+    pthread_barrier_destroy(&mybarrier);
+
+    struct timeval phase_3{};
+    gettimeofday(&phase_3, nullptr);
+    cout << "Time taken for phase 3: " << timeDiff(phase_3, phase_2) << " microseconds" << endl;
+
+#ifdef DEBUG
+    for (int i = 0; i < threadCount; i++) {
+        cout << "Re assigned partitions: thread " << i << endl;
+        for (auto p: globalPartitions[i]) {
+            for (long j = p.first; j < p.second + 1; j++) {
+                cout << values[j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+#endif
+
+    pthread_barrier_init(&mybarrier, nullptr, threadCount);
+    for (int i = 0; i < threadCount; i++) {
+        pthread_create(&(ThreadID[i]), nullptr, successiveMerge, (void *)&TCB[i]);
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(ThreadID[i], nullptr);
+    }
+
+    pthread_barrier_destroy(&mybarrier);
+    struct timeval phase_4{};
+    gettimeofday(&phase_4, nullptr);
+    cout << "Time taken for phase 4: " << timeDiff(phase_4, phase_3) << " microseconds" << endl;
+
+    vector<long> final_vector;
+    for (int i = 0; i < threadCount; i++) {
+        final_vector.insert(final_vector.end(), TCB[i].mergedPartition.begin(), TCB[i].mergedPartition.end());
+    }
+
+#ifdef DEBUG
+    cout << "After Merge" <<endl;
+    for (size_t i = 0; i < final_vector.size(); ++i) {
+        cout << final_vector[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Sorted " << endl;
+    for (long i = 0; i < arraySize; i++) {
+        cout << arr_copy[i] << " ";
+    }
+    cout << endl;
+#endif
+
+    bool equivalent = true;
+    if (final_vector.size() == arraySize) {
+
+        for (size_t i = 0; i < final_vector.size(); ++i) {
+                if (final_vector[i] != *(arr_copy + i)) {
+                    equivalent = false;
+                    break;
+                }
+        }
+        cout << endl;
+    }
+    else {
+        equivalent = false;
+    }
+
+    // Print the result
+    if (equivalent) {
+        std::cout << "The final output is equal to sequential sort result." << std::endl;
+    } else {
+        std::cout << "The final output is NOT equal to sequential sort result." << std::endl;
+    }
+
+    // free memory
+    free(values);
+    free(arr_copy);
 }
