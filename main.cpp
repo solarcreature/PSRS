909437391613291ac18bfb2edfcac1f1c01ffee6
Sanjeev Kotha <kothasanju@gmail.com>
2024-02-06 00:39:49 -0700

    Finished phase 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <sys/time.h>
 using namespace std;
 
@@ -73,13 +72,14 @@ struct ThreadControlBlock {
     long *localBlock;
     long blockSize;
     long *regularSample;
+    vector<long> pivots;
+    vector<pair<int, int>> partitions;
 };
 
 long timeDiff (timeval tv2, timeval tv) {
     long microseconds = (tv2.tv_sec - tv.tv_sec) * 1000000 + ((int)tv2.tv_usec - (int)tv.tv_usec);
     return microseconds;
 }
-
 
 int comparator(const void * p, const void * q) {
     long * l = (long *) p;
@@ -91,6 +91,23 @@ int comparator(const void * p, const void * q) {
         return 0;
     else
         return -1;
+}
+
+long binary_search(long *block, long size, long pivot) {
+    long start = 0;
+    long end = size - 1;
+    long mid;
+
+    while (start <= end) {
+        mid = (start + end) / 2;
+        if (block[mid] == pivot)
+            return mid;
+        else if (block[mid] < pivot)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return end;
 }
 
 void * sortAndSampleLocal(void * arg) {
@@ -111,6 +128,24 @@ void * sortAndSampleLocal(void * arg) {
     pthread_exit(nullptr);
 }
 
+void *createPartitions(void *arg) {
+    auto *myTCB = (struct ThreadControlBlock *)arg;
+    long *block = myTCB->localBlock;
+    long start = 0;
+    long end;
+
+    for (long pivot : myTCB->pivots) {
+        end = binary_search(block, myTCB->blockSize, pivot);
+        myTCB->partitions.emplace_back(start, end);
+        start = end + 1;
+    }
+
+    myTCB->partitions.emplace_back(start, myTCB->blockSize - 1);
+
+    pthread_barrier_wait(&mybarrier);
+    pthread_exit(nullptr);
+}
+
 int main(int argc, char ** argv) {
 
     if (argc < 3) {
@@ -122,16 +157,15 @@ int main(int argc, char ** argv) {
     // Setting parameters
     arraySize = stol(argv[1]);
     threadCount = stoi(argv[2]);
-    vector <long> data(arraySize);
+    auto * data = new long [arraySize];
     struct ThreadControlBlock TCB[threadCount];
     pthread_t ThreadID[threadCount];
     long blockSize = floor(arraySize / threadCount);
     pthread_setconcurrency(threadCount);
-
     // "Randomly" generating data
     srandom((unsigned) time(nullptr));
     for (long i = 0; i < arraySize; i++) {
-        data.push_back((long) random());
+        data[i] = (long) (random());
     }
 
     struct timeval start_time{};
@@ -161,4 +195,37 @@ int main(int argc, char ** argv) {
     struct timeval phase_1{};
     gettimeofday(&phase_1, nullptr);
     cout << "Time taken for phase 1: " << timeDiff(phase_1, start_time) << " microseconds" << endl;
+
+    vector<long> localSamples;
+
+    for (int i = 0; i < threadCount; i++) {
+        localSamples.insert(localSamples.end(), TCB[i].regularSample, TCB[i].regularSample + threadCount);
+        delete TCB[i].regularSample;
+    }
+
+    sort(localSamples.begin(), localSamples.end());
+
+    long rho = floor(threadCount / 2);
+    vector<long> pivots;
+
+    for (int i = 1; i < threadCount; i++) {
+        pivots.push_back(localSamples[i * threadCount + rho]);
+    }
+
+    pthread_barrier_init(&mybarrier, nullptr, threadCount);
+
+    for (int i = 0; i < threadCount; i++) {
+        TCB[i].pivots = pivots;
+        pthread_create(&(ThreadID[i]), nullptr, createPartitions, (void *)&TCB[i]);
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(ThreadID[i], nullptr);
+    }
+
+    pthread_barrier_destroy(&mybarrier);
+
+    struct timeval phase_2{};
+    gettimeofday(&phase_2, nullptr);
+    cout << "Time taken for phase 2: " << timeDiff(phase_2, phase_1) << " microseconds" << endl;
 }
